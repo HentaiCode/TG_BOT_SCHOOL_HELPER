56353c7c295c890dbf97a79450a425ba8defe3cc
dmtnm <dmtnmn@gmail.com>
2022-04-28 21:29:53 +0300

    you can't open link '/addjob' if you are not teacher

--- blueprints/jobs.py
+++ blueprints/jobs.py
@@ -4,6 +4,7 @@ from werkzeug.utils import redirect
 from forms.addjob import AddJobForm
 from database.models.jobs import Jobs as Job
 from database.repositories import jobs_rep
+from flask_login import current_user
 
 bp = Blueprint('jobs', __name__)
 
@@ -11,10 +12,13 @@ bp = Blueprint('jobs', __name__)
 @bp.route('/addjob', methods=['GET', 'POST'])
 def addjob():
     form = AddJobForm()
-    if form.validate_on_submit():
-        job = Job()
-        form.populate_obj(job)
-        jobs_rep.save_job(job)
+    if current_user.__dict__:
+        if current_user.position == 'Учитель':
+            if form.validate_on_submit():
+                job = Job()
+                form.populate_obj(job)
+                jobs_rep.save_job(job)
 
-        return redirect('/')
-    return render_template('addjob.html', title='Добавление работы', form=form)
+                return redirect('/')
+            return render_template('addjob.html', title='Добавление работы', form=form)
+    return {"error": "Access denied"}
